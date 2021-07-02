99e54621d1b804f023f6cbc6ddd7aea627aa0c84
Andreas Fritzler <andreas.fritzler@sap.com>
2021-07-02 15:57:06 +0200

    Added accounts type and controller and some cleanup

---
+++ apis/core/v1alpha1/account_types.go
@@ -0,0 +1,84 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// AccountSpec defines the desired state of Account
+type AccountSpec struct {
+	// CreatedBy is a subject representing a user name, an email address, or any other identifier of a user
+	// who created the project.
+	CreatedBy *rbacv1.Subject `json:"createdBy,omitempty"`
+	// Description is a human-readable description of what the project is used for.
+	Description *string `json:"description,omitempty"`
+	// Owner is a subject representing a user name, an email address, or any other identifier of a user owning
+	// the project.
+	Owner *rbacv1.Subject `json:"owner,omitempty"`
+	// Purpose is a human-readable explanation of the project's purpose.
+	Purpose *string `json:"purpose,omitempty"`
+}
+
+// AccountStatus defines the observed state of Account
+type AccountStatus struct {
+	// State represents the state of the account
+	State State `json:"state,omitempty"`
+	// Namespace references the namespace of the account
+	Namespace string `json:"namespace,omitempty"`
+}
+
+// State is a label for the condition of a project at the current time.
+type State string
+
+const (
+	// AccountPending indicates that the account reconciliation is pending.
+	AccountPending State = "Pending"
+	// AccountReady indicates that the account reconciliation was successful.
+	AccountReady State = "Ready"
+	// AccountFailed indicates that the account reconciliation failed.
+	AccountFailed State = "Failed"
+	// AccountTerminating indicates that the account is in termination process.
+	AccountTerminating State = "Terminating"
+)
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:resource:scope=Cluster
+
+// Account is the Schema for the accounts API
+type Account struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   AccountSpec   `json:"spec,omitempty"`
+	Status AccountStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// AccountList contains a list of Account
+type AccountList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Account `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Account{}, &AccountList{})
+}
