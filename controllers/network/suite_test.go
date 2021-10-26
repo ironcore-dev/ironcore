c5c4f3d315c49992e9d3dabb6a79dae1faca5f80
Lim, Kian-An <lim.kian.an@gmx.de>
2021-10-26 11:03:40 +0200

    Remove custom reporter

--- controllers/network/suite_test.go
+++ controllers/network/suite_test.go
@@ -28,7 +28,6 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -46,9 +45,7 @@ var (
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
 
-	RunSpecsWithDefaultAndCustomReporters(t,
-		"Newwork Controller Suite",
-		[]Reporter{printer.NewlineReporter{}})
+	RunSpecs(t, "Newwork Controller Suite")
 }
 
 var _ = BeforeSuite(func() {
