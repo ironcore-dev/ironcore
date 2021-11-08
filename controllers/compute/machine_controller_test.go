75a72f91ceb5eaa63628e90d7a5811caa9edbd4c
Lim, Kian-An <lim.kian.an@gmx.de>
2021-11-08 17:11:41 +0100

    Show interfaces in the test case

--- controllers/compute/machine_controller_test.go
+++ controllers/compute/machine_controller_test.go
@@ -23,39 +23,52 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"inet.af/netaddr"
+	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	"github.com/onmetal/onmetal-api/apis/common/v1alpha1"
+	commonv1alpha1 "github.com/onmetal/onmetal-api/apis/common/v1alpha1"
 	computev1alpha1 "github.com/onmetal/onmetal-api/apis/compute/v1alpha1"
 	networkv1alpha1 "github.com/onmetal/onmetal-api/apis/network/v1alpha1"
 )
 
 var _ = Describe("machine controller", func() {
 	It("reconciles a machine with interfaces", func() {
-		m := newMachineWithInterfaces("with-interface")
-		rngs, rngKeys := newIPAMRanges(m)
+		m := newMachine("with-interfaces")
+		ifaces := []computev1alpha1.Interface{
+			{
+				Name:   m.Name + "-0",
+				IP:     ip("192.168.0.0"),
+				Target: corev1.LocalObjectReference{Name: "target-0"},
+			},
+			{
+				Name:   m.Name + "-1",
+				IP:     ip("192.168.0.1"),
+				Target: corev1.LocalObjectReference{Name: "target-1"},
+			},
+		}
+		m.Spec.Interfaces = ifaces
 
 		By("creating the machine")
 		Expect(k8sClient.Create(ctx, m)).To(Succeed())
 
-		for i := range rngs {
+		rngs := newIPAMRanges(m)
+		for i, rng := range rngs {
 			By("fetching the corresponding IPAMRange")
 			Eventually(func() error {
-				return notFoundOrSucceed(k8sClient.Get(ctx, rngKeys[i], rngs[i]))
+				return notFoundOrSucceed(k8sClient.Get(ctx, objectKey(rng), rng))
 			}, timeout, interval).Should(Succeed())
 
 			By("checking if the parent of the IPAMRange corresponds to the target of the interface")
-			Expect(rngs[i].Spec.Parent.Name).To(Equal(networkv1alpha1.SubnetIPAMName(ifaceTargets[i])))
+			Expect(rng.Spec.Parent.Name).To(Equal(networkv1alpha1.SubnetIPAMName(ifaces[i].Target.Name)))
 
 			By("checking if the request of the IPAMRange corresponds to the IP of the machine's interface")
-			Expect(rngs[i].Spec.Requests[0]).To(Equal(*ipamRangeRequest(ifaceIPs[i])))
+			Expect(rng.Spec.Requests[0]).To(Equal(ipamRangeRequest(ifaces[i].IP)))
 
 			By("checking the OwnerReferences of the IPAMRange contain the machine")
-			Expect(rngs[i].OwnerReferences).To(ContainElement(controllerReference(m)))
+			Expect(rng.OwnerReferences).To(ContainElement(controllerReference(m)))
 		}
 	})
 })
@@ -70,10 +83,7 @@ const (
 )
 
 var (
-	// test data
-	ifaceTargets = []string{"0", "1"}
-	ifaceIPs     = []string{"192.168.0.0", "192.168.0.1"}
-	objectKey    = client.ObjectKeyFromObject
+	objectKey = client.ObjectKeyFromObject
 )
 
 func controllerReference(m *computev1alpha1.Machine) metav1.OwnerReference {
@@ -87,36 +97,23 @@ func controllerReference(m *computev1alpha1.Machine) metav1.OwnerReference {
 	}
 }
 
-func ipamRangeRequest(ip string) *networkv1alpha1.IPAMRangeRequest {
-	parsedIP, _ := netaddr.ParseIP(ip)
-	return &networkv1alpha1.IPAMRangeRequest{
-		IPs: v1alpha1.NewIPRangePtr(netaddr.IPRangeFrom(parsedIP, parsedIP)),
-	}
-}
-
-func newInterface(name, ip, target string) *computev1alpha1.Interface {
-	iface := &computev1alpha1.Interface{}
-	iface.Name = name
+func ip(ip string) *commonv1alpha1.IPAddr {
 	parsed, _ := netaddr.ParseIP(ip)
-	iface.IP = v1alpha1.NewIPAddrPtr(parsed)
-	iface.Target.Name = target
-	return iface
+	return commonv1alpha1.NewIPAddrPtr(parsed)
 }
 
-func newInterfaces(m *computev1alpha1.Machine) []computev1alpha1.Interface {
-	return []computev1alpha1.Interface{
-		*newInterface(m.Name+"-0", ifaceIPs[0], ifaceTargets[0]),
-		*newInterface(m.Name+"-1", ifaceIPs[1], ifaceTargets[1]),
+func ipamRangeRequest(ip *commonv1alpha1.IPAddr) networkv1alpha1.IPAMRangeRequest {
+	return networkv1alpha1.IPAMRangeRequest{
+		IPs: commonv1alpha1.NewIPRangePtr(netaddr.IPRangeFrom(ip.IP, ip.IP)),
 	}
 }
 
-func newIPAMRanges(m *computev1alpha1.Machine) (rngs []*networkv1alpha1.IPAMRange, keys []types.NamespacedName) {
+func newIPAMRanges(m *computev1alpha1.Machine) (rngs []*networkv1alpha1.IPAMRange) {
 	for _, iface := range m.Spec.Interfaces {
 		rng := &networkv1alpha1.IPAMRange{}
 		rng.Namespace = m.Namespace
 		rng.Name = computev1alpha1.MachineInterfaceIPAMRangeName(m.Name, iface.Name)
 		rngs = append(rngs, rng)
-		keys = append(keys, objectKey(rng))
 	}
 	return
 }
@@ -127,12 +124,6 @@ func newMachine(name string) *computev1alpha1.Machine {
 	m.Kind = kind
 	m.Namespace = ns.Name
 	m.Name = name
-	return m
-}
-
-func newMachineWithInterfaces(name string) *computev1alpha1.Machine {
-	m := newMachine(name)
-	m.Spec.Interfaces = newInterfaces(m)
 	return m
 }
 
