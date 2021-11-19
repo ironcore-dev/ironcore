f2a7e92d33cb44c90060269756d7d615db346a8f
Lim, Kian-An <lim.kian.an@gmx.de>
2021-11-19 21:04:31 +0100

    Check the finalizer directly

--- controllers/compute/machineclass_controller_test.go
+++ controllers/compute/machineclass_controller_test.go
@@ -17,11 +17,12 @@
 package compute
 
 import (
-	"fmt"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -31,9 +32,9 @@ import (
 var _ = Describe("machineclass controller", func() {
 	ns := SetupTest(ctx)
 	It("signals error upon machineclass deletion if there's any machine still using the machineclass", func() {
+		time.Sleep(1 * time.Second)
 		mClass := &computev1alpha1.MachineClass{
 			ObjectMeta: metav1.ObjectMeta{
-				Namespace:    ns.Name,
 				GenerateName: "machineclass-",
 			},
 			Spec: computev1alpha1.MachineClassSpec{},
@@ -53,15 +54,19 @@ var _ = Describe("machineclass controller", func() {
 		}
 		Expect(k8sClient.Create(ctx, m)).Should(Succeed())
 
-		mm := &computev1alpha1.MachineList{}
-		if err := k8sClient.List(ctx, mm, client.InNamespace(mClass.Namespace), client.MatchingFields{machineClassNameField: mClass.Name}); err != nil {
-			fmt.Fprintf(GinkgoWriter, "\n===> %#v\n\n", err)
-		}
-
-		Expect(k8sClient.Delete(ctx, mClass)).Should(Equal(errMachineClassDeletionForbidden))
-
-		Expect(k8sClient.Delete(ctx, m)).Should(Succeed())
 		Expect(k8sClient.Delete(ctx, mClass)).Should(Succeed())
+
+		// Check the finalizer is still there
+		mClassKey := objKey(mClass)
+		Expect(k8sClient.Get(ctx, mClassKey, mClass))
+		Expect(mClass.Finalizers).To(ContainElement(computev1alpha1.MachineClassFinalizer))
+
+		// Eventually the machineclass is gone
+		Expect(k8sClient.Delete(ctx, m)).Should(Succeed())
+		Eventually(func() bool {
+			err := k8sClient.Get(ctx, mClassKey, mClass)
+			return apierrors.IsNotFound(err)
+		}, timeout, interval).Should(BeTrue(), "error equal to `not found`")
 	})
 })
 
