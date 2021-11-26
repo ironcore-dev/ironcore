3c180695ff717ee209ee2cf2e355c49c647858b5
Lim, Kian-An <lim.kian.an@gmx.de>
2021-11-26 18:26:17 +0100

    Test more thoroughly

--- apis/compute/v1alpha1/machine_webhook_test.go
+++ apis/compute/v1alpha1/machine_webhook_test.go
@@ -19,13 +19,15 @@ package v1alpha1
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
 var _ = Describe("machine validation webhook", func() {
 	ns := SetupTest()
 	Context("upon machine update", func() {
-		It("validates if machinepool is changed", func() {
+		It("signals error if machinepool is changed", func() {
 			m := &Machine{
 				ObjectMeta: metav1.ObjectMeta{
 					Namespace:    ns.Name,
@@ -34,8 +36,29 @@ var _ = Describe("machine validation webhook", func() {
 			}
 			Expect(k8sClient.Create(ctx, m)).To(Succeed())
 
-			m.Spec.MachinePool.Name = "another machinepool"
-			Expect(k8sClient.Update(ctx, m)).NotTo(Succeed())
+			newMachinePoolName := "another machinepool"
+			m.Spec.MachinePool.Name = newMachinePoolName
+			err := k8sClient.Update(ctx, m)
+			Expect(err).To(HaveOccurred())
+
+			path := field.NewPath("spec", "machinePool", "name")
+			fieldErr := field.Invalid(path, newMachinePoolName, "machinepool should be immutable")
+			fieldErrList := field.ErrorList{fieldErr}
+			Expect(err.Error()).To(ContainSubstring(fieldErrList.ToAggregate().Error()))
+		})
+
+		It("keeps silent when machinepool isn't changed", func() {
+			m := &Machine{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace:    ns.Name,
+					GenerateName: "machine-",
+				},
+			}
+			Expect(k8sClient.Create(ctx, m)).To(Succeed())
+
+			m.Spec.SecurityGroups = []corev1.LocalObjectReference{{Name: "new-security-group"}}
+			err := k8sClient.Update(ctx, m)
+			Expect(err).ToNot(HaveOccurred())
 		})
 	})
 })
