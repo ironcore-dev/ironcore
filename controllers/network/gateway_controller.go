267116d61ed5e93932604157570fd8938996ba56
Lim, Kian-An <lim.kian.an@gmx.de>
2021-12-08 09:27:02 +0100

    Request an IP address to the Gateway (#203)

--- controllers/network/gateway_controller.go
+++ controllers/network/gateway_controller.go
@@ -18,14 +18,20 @@ package network
 
 import (
 	"context"
+	"fmt"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	networkv1alpha1 "github.com/onmetal/onmetal-api/apis/network/v1alpha1"
+	"github.com/onmetal/onmetal-api/predicates"
 )
+
+const gatewayFieldOwner = client.FieldOwner("compute.onmetal.de/gateway")
 
 // GatewayReconciler reconciles a Gateway object
 type GatewayReconciler struct {
@@ -36,27 +42,70 @@ type GatewayReconciler struct {
 //+kubebuilder:rbac:groups=network.onmetal.de,resources=gateways,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=network.onmetal.de,resources=gateways/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=network.onmetal.de,resources=gateways/finalizers,verbs=update
+//+kubebuilder:rbac:groups=network.onmetal.de,resources=ipamranges,verbs=get;list;watch;create;update;patch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Gateway object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
+// Reconcile moves the current state of the cluster closer to the desired state.
 func (r *GatewayReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	// Get the Gateway identified in the reg
+	gw := &networkv1alpha1.Gateway{}
+	if err := r.Get(ctx, req.NamespacedName, gw); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
-	// your logic here
+	// Each Gateway needs an IP address, which can be acquired via a corresponding IPAMRange.
+	ipamRange := newIPAMRangeFromGateway(gw)
 
-	return ctrl.Result{}, nil
+	// The Gateway is the controller (the top owner upon conflicts with other owners) of its corresponding IPAMRange.
+	if err := ctrl.SetControllerReference(gw, ipamRange, r.Scheme); err != nil {
+		return ctrl.Result{}, fmt.Errorf("setting the controller reference of the ipam range: %w", err)
+	}
+
+	// Server-side apply (create or update) the corrresponding IPAMRange
+	if err := r.Patch(ctx, ipamRange, client.Apply, gatewayFieldOwner); err != nil {
+		return ctrl.Result{}, fmt.Errorf("server-side applying the ipam range: %w", err)
+	}
+
+	return r.updateGatewayStatus(ctx, gw, ipamRange)
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *GatewayReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&networkv1alpha1.Gateway{}).
+		Owns(&networkv1alpha1.IPAMRange{}, builder.WithPredicates(predicates.IPAMRangeAllocationsChangedPredicate{})).
 		Complete(r)
+}
+
+func (r *GatewayReconciler) updateGatewayStatus(ctx context.Context, gw *networkv1alpha1.Gateway, ipamRange *networkv1alpha1.IPAMRange) (ctrl.Result, error) {
+	// Check if the IPAMRange is reconciled. If not, requeue the Gateway.
+	if len(ipamRange.Status.Allocations) == 0 {
+		return ctrl.Result{}, nil
+	}
+
+	// Update the status of the Gateway.
+	oldGW := gw.DeepCopy()
+	gw.Status.IPs = append(gw.Status.IPs, ipamRange.Status.Allocations[0].IPs.From)
+	if err := r.Status().Patch(ctx, gw, client.MergeFrom(oldGW)); err != nil {
+		return ctrl.Result{}, fmt.Errorf("updating status: %w", err)
+	}
+	return ctrl.Result{}, nil
+}
+
+func newIPAMRangeFromGateway(gw *networkv1alpha1.Gateway) *networkv1alpha1.IPAMRange {
+	return &networkv1alpha1.IPAMRange{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: networkv1alpha1.GroupVersion.String(),
+			Kind:       networkv1alpha1.IPAMRangeGK.Kind,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: gw.Namespace,
+			Name:      networkv1alpha1.GatewayIPAMRangeName(gw),
+		},
+		Spec: networkv1alpha1.IPAMRangeSpec{
+			Parent: &corev1.LocalObjectReference{
+				Name: networkv1alpha1.SubnetIPAMName(gw.Spec.Subnet.Name),
+			},
+			Requests: []networkv1alpha1.IPAMRangeRequest{{IPCount: 1}},
+		},
+	}
 }
