509c15285480efc942782375f08eb97eb046925f
Lim, Kian-An <lim.kian.an@gmx.de>
2021-12-10 14:33:21 +0100

    `MachineClass` and `StorageClass` should have Finalizers (#210)
    Signed-off-by: Lim, Kian-An <lim.kian.an@gmx.de>

--- controllers/storage/storageclass_controller.go
+++ controllers/storage/storageclass_controller.go
@@ -18,14 +18,24 @@ package storage
 
 import (
 	"context"
+	"fmt"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/workqueue"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	"github.com/go-logr/logr"
 
 	storagev1alpha1 "github.com/onmetal/onmetal-api/apis/storage/v1alpha1"
 )
+
+const storageClassNameField = ".spec.storageclass.name"
 
 // StorageClassReconciler reconciles a StorageClass object
 type StorageClassReconciler struct {
@@ -37,26 +47,99 @@ type StorageClassReconciler struct {
 //+kubebuilder:rbac:groups=storage.onmetal.de,resources=storageclasses/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=storage.onmetal.de,resources=storageclasses/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the StorageClass object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
+// Reconcile moves the current state of the cluster closer to the desired state.
 func (r *StorageClassReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := ctrl.LoggerFrom(ctx)
+	storageclass := &storagev1alpha1.StorageClass{}
+	if err := r.Get(ctx, req.NamespacedName, storageclass); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
-	// your logic here
-
-	return ctrl.Result{}, nil
+	return r.reconcileExists(ctx, log, storageclass)
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *StorageClassReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	// Index the field of storageclass name for listing volumes in storageclass controller
+	if err := mgr.GetFieldIndexer().IndexField(
+		context.Background(),
+		&storagev1alpha1.Volume{},
+		storageClassNameField,
+		func(object client.Object) []string {
+			m := object.(*storagev1alpha1.Volume)
+			if m.Spec.StorageClass.Name == "" {
+				return nil
+			}
+			return []string{m.Spec.StorageClass.Name}
+		},
+	); err != nil {
+		return fmt.Errorf("indexing the field %s: %w", storageClassNameField, err)
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&storagev1alpha1.StorageClass{}).
+		Watches(
+			&source.Kind{Type: &storagev1alpha1.Volume{}},
+			handler.Funcs{
+				DeleteFunc: func(e event.DeleteEvent, q workqueue.RateLimitingInterface) {
+					v := e.Object.(*storagev1alpha1.Volume)
+					q.Add(ctrl.Request{NamespacedName: types.NamespacedName{Name: v.Spec.StorageClass.Name}})
+				},
+			},
+		).
 		Complete(r)
+}
+
+func (r *StorageClassReconciler) delete(ctx context.Context, log logr.Logger, storageclass *storagev1alpha1.StorageClass) (ctrl.Result, error) {
+	if !controllerutil.ContainsFinalizer(storageclass, storagev1alpha1.StorageClassFinalizer) {
+		return ctrl.Result{}, nil
+	}
+
+	// List the volumes currently using the storageclass
+	volumeList := &storagev1alpha1.VolumeList{}
+	if err := r.List(ctx, volumeList, client.InNamespace(storageclass.Namespace), client.MatchingFields{storageClassNameField: storageclass.Name}); err != nil {
+		return ctrl.Result{}, fmt.Errorf("listing the volumes using the storageclass: %w", err)
+	}
+
+	// Check if there's still any volume using the storageclass
+	if len(volumeList.Items) != 0 {
+		// List the names of the volumes still using the storageclass
+		volumeNames := make([]string, 0, len(volumeList.Items))
+		for _, volume := range volumeList.Items {
+			volumeNames = append(volumeNames, volume.Name)
+		}
+		log.Info("Forbidden to delete the volumeclass which is still used by volumes", "volume names", volumeNames)
+
+		return ctrl.Result{}, nil
+	}
+
+	// Remove the finalizer in the storageclass and persist the new state
+	old := storageclass.DeepCopy()
+	controllerutil.RemoveFinalizer(storageclass, storagev1alpha1.StorageClassFinalizer)
+	if err := r.Patch(ctx, storageclass, client.MergeFrom(old)); err != nil {
+		return ctrl.Result{}, fmt.Errorf("removing the finalizer: %w", err)
+	}
+	log.V(1).Info("Successfully removed finalizer")
+
+	return ctrl.Result{}, nil
+}
+
+func (r *StorageClassReconciler) reconcile(ctx context.Context, log logr.Logger, storageclass *storagev1alpha1.StorageClass) (ctrl.Result, error) {
+	if !controllerutil.ContainsFinalizer(storageclass, storagev1alpha1.StorageClassFinalizer) {
+		old := storageclass.DeepCopy()
+		controllerutil.AddFinalizer(storageclass, storagev1alpha1.StorageClassFinalizer)
+		if err := r.Patch(ctx, storageclass, client.MergeFrom(old)); err != nil {
+			return ctrl.Result{}, fmt.Errorf("adding the finalizer: %w", err)
+		}
+		return ctrl.Result{}, nil
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *StorageClassReconciler) reconcileExists(ctx context.Context, log logr.Logger, storageclass *storagev1alpha1.StorageClass) (ctrl.Result, error) {
+	if !storageclass.DeletionTimestamp.IsZero() {
+		return r.delete(ctx, log, storageclass)
+	}
+	return r.reconcile(ctx, log, storageclass)
 }
