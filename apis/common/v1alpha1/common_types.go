50ac1333dcc8fa9c05db32a4e7e4aa536c3fda5a
Lim, Kian-An <lim.kian.an@gmx.de>
2022-02-03 16:02:28 +0100

    Port k8s funcs for Taint

--- apis/common/v1alpha1/common_types.go
+++ apis/common/v1alpha1/common_types.go
@@ -18,6 +18,7 @@ package v1alpha1
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"inet.af/netaddr"
 	corev1 "k8s.io/api/core/v1"
@@ -263,6 +264,28 @@ type Taint struct {
 	// that do not tolerate the taint.
 	// Valid effects are NoSchedule, PreferNoSchedule and NoExecute.
 	Effect TaintEffect `json:"effect"`
+}
+
+// From https://pkg.go.dev/k8s.io/api/core/v1#Taint.MatchTaint
+// MatchTaint checks if the taint matches taintToMatch. Taints are unique by key:effect,
+// if the two taints have same key:effect, regard as they match.
+func (t *Taint) MatchTaint(taintToMatch *Taint) bool {
+	return t.Key == taintToMatch.Key && t.Effect == taintToMatch.Effect
+}
+
+// From https://pkg.go.dev/k8s.io/api/core/v1#Taint.ToString
+// ToString converts taint struct to string in format '<key>=<value>:<effect>', '<key>=<value>:', '<key>:<effect>', or '<key>'.
+func (t *Taint) ToString() string {
+	if len(t.Effect) == 0 {
+		if len(t.Value) == 0 {
+			return fmt.Sprintf("%v", t.Key)
+		}
+		return fmt.Sprintf("%v=%v:", t.Key, t.Value)
+	}
+	if len(t.Value) == 0 {
+		return fmt.Sprintf("%v:%v", t.Key, t.Effect)
+	}
+	return fmt.Sprintf("%v=%v:%v", t.Key, t.Value, t.Effect)
 }
 
 //+kubebuilder:validation:Enum=NoSchedule
