bcdea17f7a08f8a724899920dc90a7dfe6520494
Axel Christ <axel.christ@sap.com>
2022-04-08 17:31:10 +0200

    Fix `validate_kustomize.sh`

--- hack/validate_kustomize.sh
+++ hack/validate_kustomize.sh
@@ -4,9 +4,10 @@ set -e
 
 BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-for dir in "$BASEDIR"/../config/*
-do
+for dir in "$BASEDIR"/../config/**/*; do
   [[ -e "$dir" ]] || break
+  [[ -f "$dir/kustomization.yaml" ]] || break
   [[ "$dir" != *"config/samples"* ]] || break
+  echo "Validating $dir"
   kustomize build "$dir" > /dev/null
 done
