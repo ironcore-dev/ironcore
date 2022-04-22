onmetal/onmetal-api

Add `NetworkInterface` type
- Add sample manifest

apis/compute/networkinterface_types.go
@@ -1,0 +1,79 @@
+/*
+ * Copyright (c) 2022 by the OnMetal authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package compute
+
+import (
+	commonv1alpha1 "github.com/onmetal/onmetal-api/apis/common/v1alpha1"
+	ipamv1alpha1 "github.com/onmetal/onmetal-api/apis/ipam/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// NetworkInterfaceSpec defines the desired state of NetworkInterface
+type NetworkInterfaceSpec struct {
+	// NetworkRef is the Network this NetworkInterface is connected to
+	NetworkRef corev1.LocalObjectReference
+	// MachineRef is the Machine this NetworkInterface is used by
+	MachineRef corev1.LocalObjectReference
+	// IPFamilies defines which IPFamilies this NetworkInterface is supporting
+	IPFamilies []corev1.IPFamily
+	// IPs is the list of provided IPs or EphemeralIPs which should be assigned to
+	// this NetworkInterface
+	IPs []IPSpec
+}
+
+type IPSpec struct {
+	Value           *commonv1alpha1.IP
+	EphemeralPrefix *EphemeralPrefixSource
+}
+
+type EphemeralPrefixSource struct {
+	PrefixTemplate *PrefixTemplate
+}
+
+type PrefixTemplate struct {
+	metav1.ObjectMeta
+	Spec ipamv1alpha1.PrefixSpec
+}
+
+// NetworkInterfaceStatus defines the observed state of NetworkInterface
+type NetworkInterfaceStatus struct {
+	// TODO: Add State, Conditions
+	// IPs represent the effective IP addresses of the NetworkInterface
+	IPs []commonv1alpha1.IP
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// NetworkInterface is the Schema for the networkinterfaces API
+type NetworkInterface struct {
+	metav1.TypeMeta
+	metav1.ObjectMeta
+
+	Spec   NetworkInterfaceSpec
+	Status NetworkInterfaceStatus
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// NetworkInterfaceList contains a list of NetworkInterface
+type NetworkInterfaceList struct {
+	metav1.TypeMeta
+	metav1.ListMeta
+	Items []NetworkInterface
+}

apis/compute/v1alpha1/networkinterface_types.go
@@ -1,0 +1,79 @@
+/*
+ * Copyright (c) 2022 by the OnMetal authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1alpha1
+
+import (
+	commonv1alpha1 "github.com/onmetal/onmetal-api/apis/common/v1alpha1"
+	ipamv1alpha1 "github.com/onmetal/onmetal-api/apis/ipam/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// NetworkInterfaceSpec defines the desired state of NetworkInterface
+type NetworkInterfaceSpec struct {
+	// NetworkRef is the Network this NetworkInterface is connected to
+	NetworkRef corev1.LocalObjectReference `json:"networkRef"`
+	// MachineRef is the Machine this NetworkInterface is used by
+	MachineRef corev1.LocalObjectReference `json:"machineRef,omitempty"`
+	// IPFamilies defines which IPFamilies this NetworkInterface is supporting
+	IPFamilies []corev1.IPFamily `json:"ipFamilies"`
+	// IPs is the list of provided IPs or EphemeralIPs which should be assigned to
+	// this NetworkInterface
+	IPs []IPSpec `json:"ips"`
+}
+
+type IPSpec struct {
+	Value           *commonv1alpha1.IP     `json:"value,omitempty"`
+	EphemeralPrefix *EphemeralPrefixSource `json:"ephemeralPrefix,omitempty"`
+}
+
+type EphemeralPrefixSource struct {
+	PrefixTemplate *PrefixTemplate `json:"prefixTemplate,omitempty"`
+}
+
+type PrefixTemplate struct {
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	Spec              ipamv1alpha1.PrefixSpec `json:"spec,omitempty"`
+}
+
+// NetworkInterfaceStatus defines the observed state of NetworkInterface
+type NetworkInterfaceStatus struct {
+	// TODO: Add State, Conditions
+	// IPs represent the effective IP addresses of the NetworkInterface
+	IPs []commonv1alpha1.IP `json:"ips,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// NetworkInterface is the Schema for the networkinterfaces API
+type NetworkInterface struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   NetworkInterfaceSpec   `json:"spec,omitempty"`
+	Status NetworkInterfaceStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// NetworkInterfaceList contains a list of NetworkInterface
+type NetworkInterfaceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []NetworkInterface `json:"items"`
+}

apis/compute/zz_generated.deepcopy.go
@@ -43,6 +43,52 @@ func (in *EFIVar) DeepCopy() *EFIVar {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *EphemeralPrefixSource) DeepCopyInto(out *EphemeralPrefixSource) {
+	*out = *in
+	if in.PrefixTemplate != nil {
+		in, out := &in.PrefixTemplate, &out.PrefixTemplate
+		*out = new(PrefixTemplate)
+		(*in).DeepCopyInto(*out)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new EphemeralPrefixSource.
+func (in *EphemeralPrefixSource) DeepCopy() *EphemeralPrefixSource {
+	if in == nil {
+		return nil
+	}
+	out := new(EphemeralPrefixSource)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *IPSpec) DeepCopyInto(out *IPSpec) {
+	*out = *in
+	if in.Value != nil {
+		in, out := &in.Value, &out.Value
+		*out = (*in).DeepCopy()
+	}
+	if in.EphemeralPrefix != nil {
+		in, out := &in.EphemeralPrefix, &out.EphemeralPrefix
+		*out = new(EphemeralPrefixSource)
+		(*in).DeepCopyInto(*out)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new IPSpec.
+func (in *IPSpec) DeepCopy() *IPSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(IPSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *Interface) DeepCopyInto(out *Interface) {
 	*out = *in
 	return
@@ -428,6 +474,138 @@ func (in *MachineStatus) DeepCopy() *MachineStatus {
 		return nil
 	}
 	out := new(MachineStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NetworkInterface) DeepCopyInto(out *NetworkInterface) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	in.Status.DeepCopyInto(&out.Status)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NetworkInterface.
+func (in *NetworkInterface) DeepCopy() *NetworkInterface {
+	if in == nil {
+		return nil
+	}
+	out := new(NetworkInterface)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *NetworkInterface) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NetworkInterfaceList) DeepCopyInto(out *NetworkInterfaceList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]NetworkInterface, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NetworkInterfaceList.
+func (in *NetworkInterfaceList) DeepCopy() *NetworkInterfaceList {
+	if in == nil {
+		return nil
+	}
+	out := new(NetworkInterfaceList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *NetworkInterfaceList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NetworkInterfaceSpec) DeepCopyInto(out *NetworkInterfaceSpec) {
+	*out = *in
+	out.NetworkRef = in.NetworkRef
+	out.MachineRef = in.MachineRef
+	if in.IPFamilies != nil {
+		in, out := &in.IPFamilies, &out.IPFamilies
+		*out = make([]v1.IPFamily, len(*in))
+		copy(*out, *in)
+	}
+	if in.IPs != nil {
+		in, out := &in.IPs, &out.IPs
+		*out = make([]IPSpec, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NetworkInterfaceSpec.
+func (in *NetworkInterfaceSpec) DeepCopy() *NetworkInterfaceSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(NetworkInterfaceSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NetworkInterfaceStatus) DeepCopyInto(out *NetworkInterfaceStatus) {
+	*out = *in
+	if in.IPs != nil {
+		in, out := &in.IPs, &out.IPs
+		*out = make([]v1alpha1.IP, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NetworkInterfaceStatus.
+func (in *NetworkInterfaceStatus) DeepCopy() *NetworkInterfaceStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(NetworkInterfaceStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *PrefixTemplate) DeepCopyInto(out *PrefixTemplate) {
+	*out = *in
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new PrefixTemplate.
+func (in *PrefixTemplate) DeepCopy() *PrefixTemplate {
+	if in == nil {
+		return nil
+	}
+	out := new(PrefixTemplate)
 	in.DeepCopyInto(out)
 	return out
 }

ironcore-dev/ironcore

apis/compute/networkinterface_types.go
@@ -1,0 +1,79 @@
+/*
+ * Copyright (c) 2022 by the OnMetal authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package compute
+
+import (
+	commonv1alpha1 "github.com/onmetal/onmetal-api/apis/common/v1alpha1"
+	ipamv1alpha1 "github.com/onmetal/onmetal-api/apis/ipam/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// NetworkInterfaceSpec defines the desired state of NetworkInterface
+type NetworkInterfaceSpec struct {
+	// NetworkRef is the Network this NetworkInterface is connected to
+	NetworkRef corev1.LocalObjectReference
+	// MachineRef is the Machine this NetworkInterface is used by
+	MachineRef corev1.LocalObjectReference
+	// IPFamilies defines which IPFamilies this NetworkInterface is supporting
+	IPFamilies []corev1.IPFamily
+	// IPs is the list of provided IPs or EphemeralIPs which should be assigned to
+	// this NetworkInterface
+	IPs []IPSource
+}
+
+type IPSource struct {
+	Value           *commonv1alpha1.IP
+	EphemeralPrefix *EphemeralPrefixSource
+}
+
+type EphemeralPrefixSource struct {
+	PrefixTemplate *PrefixTemplate
+}
+
+type PrefixTemplate struct {
+	metav1.ObjectMeta
+	Spec ipamv1alpha1.PrefixSpec
+}
+
+// NetworkInterfaceStatus defines the observed state of NetworkInterface
+type NetworkInterfaceStatus struct {
+	// TODO: Add State, Conditions
+	// IPs represent the effective IP addresses of the NetworkInterface
+	IPs []commonv1alpha1.IP
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// NetworkInterface is the Schema for the networkinterfaces API
+type NetworkInterface struct {
+	metav1.TypeMeta
+	metav1.ObjectMeta
+
+	Spec   NetworkInterfaceSpec
+	Status NetworkInterfaceStatus
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// NetworkInterfaceList contains a list of NetworkInterface
+type NetworkInterfaceList struct {
+	metav1.TypeMeta
+	metav1.ListMeta
+	Items []NetworkInterface
+}

apis/compute/v1alpha1/networkinterface_types.go
@@ -1,0 +1,79 @@
+/*
+ * Copyright (c) 2022 by the OnMetal authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1alpha1
+
+import (
+	commonv1alpha1 "github.com/onmetal/onmetal-api/apis/common/v1alpha1"
+	ipamv1alpha1 "github.com/onmetal/onmetal-api/apis/ipam/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// NetworkInterfaceSpec defines the desired state of NetworkInterface
+type NetworkInterfaceSpec struct {
+	// NetworkRef is the Network this NetworkInterface is connected to
+	NetworkRef corev1.LocalObjectReference `json:"networkRef"`
+	// MachineRef is the Machine this NetworkInterface is used by
+	MachineRef corev1.LocalObjectReference `json:"machineRef,omitempty"`
+	// IPFamilies defines which IPFamilies this NetworkInterface is supporting
+	IPFamilies []corev1.IPFamily `json:"ipFamilies"`
+	// IPs is the list of provided IPs or EphemeralIPs which should be assigned to
+	// this NetworkInterface
+	IPs []IPSource `json:"ips"`
+}
+
+type IPSource struct {
+	Value           *commonv1alpha1.IP     `json:"value,omitempty"`
+	EphemeralPrefix *EphemeralPrefixSource `json:"ephemeralPrefix,omitempty"`
+}
+
+type EphemeralPrefixSource struct {
+	PrefixTemplate *PrefixTemplate `json:"prefixTemplate,omitempty"`
+}
+
+type PrefixTemplate struct {
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	Spec              ipamv1alpha1.PrefixSpec `json:"spec,omitempty"`
+}
+
+// NetworkInterfaceStatus defines the observed state of NetworkInterface
+type NetworkInterfaceStatus struct {
+	// TODO: Add State, Conditions
+	// IPs represent the effective IP addresses of the NetworkInterface
+	IPs []commonv1alpha1.IP `json:"ips,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// NetworkInterface is the Schema for the networkinterfaces API
+type NetworkInterface struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   NetworkInterfaceSpec   `json:"spec,omitempty"`
+	Status NetworkInterfaceStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// NetworkInterfaceList contains a list of NetworkInterface
+type NetworkInterfaceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []NetworkInterface `json:"items"`
+}

apis/compute/zz_generated.deepcopy.go
@@ -43,6 +43,52 @@ func (in *EFIVar) DeepCopy() *EFIVar {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *EphemeralPrefixSource) DeepCopyInto(out *EphemeralPrefixSource) {
+	*out = *in
+	if in.PrefixTemplate != nil {
+		in, out := &in.PrefixTemplate, &out.PrefixTemplate
+		*out = new(PrefixTemplate)
+		(*in).DeepCopyInto(*out)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new EphemeralPrefixSource.
+func (in *EphemeralPrefixSource) DeepCopy() *EphemeralPrefixSource {
+	if in == nil {
+		return nil
+	}
+	out := new(EphemeralPrefixSource)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *IPSource) DeepCopyInto(out *IPSource) {
+	*out = *in
+	if in.Value != nil {
+		in, out := &in.Value, &out.Value
+		*out = (*in).DeepCopy()
+	}
+	if in.EphemeralPrefix != nil {
+		in, out := &in.EphemeralPrefix, &out.EphemeralPrefix
+		*out = new(EphemeralPrefixSource)
+		(*in).DeepCopyInto(*out)
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new IPSource.
+func (in *IPSource) DeepCopy() *IPSource {
+	if in == nil {
+		return nil
+	}
+	out := new(IPSource)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *Interface) DeepCopyInto(out *Interface) {
 	*out = *in
 	return
@@ -428,6 +474,138 @@ func (in *MachineStatus) DeepCopy() *MachineStatus {
 		return nil
 	}
 	out := new(MachineStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NetworkInterface) DeepCopyInto(out *NetworkInterface) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	in.Status.DeepCopyInto(&out.Status)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NetworkInterface.
+func (in *NetworkInterface) DeepCopy() *NetworkInterface {
+	if in == nil {
+		return nil
+	}
+	out := new(NetworkInterface)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *NetworkInterface) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NetworkInterfaceList) DeepCopyInto(out *NetworkInterfaceList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]NetworkInterface, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NetworkInterfaceList.
+func (in *NetworkInterfaceList) DeepCopy() *NetworkInterfaceList {
+	if in == nil {
+		return nil
+	}
+	out := new(NetworkInterfaceList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *NetworkInterfaceList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NetworkInterfaceSpec) DeepCopyInto(out *NetworkInterfaceSpec) {
+	*out = *in
+	out.NetworkRef = in.NetworkRef
+	out.MachineRef = in.MachineRef
+	if in.IPFamilies != nil {
+		in, out := &in.IPFamilies, &out.IPFamilies
+		*out = make([]v1.IPFamily, len(*in))
+		copy(*out, *in)
+	}
+	if in.IPs != nil {
+		in, out := &in.IPs, &out.IPs
+		*out = make([]IPSource, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NetworkInterfaceSpec.
+func (in *NetworkInterfaceSpec) DeepCopy() *NetworkInterfaceSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(NetworkInterfaceSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *NetworkInterfaceStatus) DeepCopyInto(out *NetworkInterfaceStatus) {
+	*out = *in
+	if in.IPs != nil {
+		in, out := &in.IPs, &out.IPs
+		*out = make([]v1alpha1.IP, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new NetworkInterfaceStatus.
+func (in *NetworkInterfaceStatus) DeepCopy() *NetworkInterfaceStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(NetworkInterfaceStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *PrefixTemplate) DeepCopyInto(out *PrefixTemplate) {
+	*out = *in
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new PrefixTemplate.
+func (in *PrefixTemplate) DeepCopy() *PrefixTemplate {
+	if in == nil {
+		return nil
+	}
+	out := new(PrefixTemplate)
 	in.DeepCopyInto(out)
 	return out
 }