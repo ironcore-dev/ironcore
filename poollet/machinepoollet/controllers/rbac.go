3692831315d65a4538b814b7b24427f983b431fc
Axel Christ <axel.christ@sap.com>
2023-03-28 16:57:46 +0200

    Add machinepoollet delegating auth rbac

--- poollet/machinepoollet/controllers/rbac.go
+++ poollet/machinepoollet/controllers/rbac.go
@@ -20,3 +20,7 @@ package controllers
 //+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;patch
 //+kubebuilder:rbac:groups=certificates.k8s.io,resources=certificatesigningrequests,verbs=create;get;list;watch
 //+kubebuilder:rbac:groups=certificates.k8s.io,resources=certificatesigningrequests/machinepoolclient,verbs=create
+
+// Rules required for machinepoollet delegated authentication
+//+kubebuilder:rbac:groups=authentication.k8s.io,resources=tokenreviews,verbs=create
+//+kubebuilder:rbac:groups=authorization.k8s.io,resources=subjectaccessreviews,verbs=create
