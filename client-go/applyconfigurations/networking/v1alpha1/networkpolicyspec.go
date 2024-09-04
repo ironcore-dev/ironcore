3fd0f1aaf9f86a23585db61073f7349c8844e71c
sujeet01 <phadtare.sujeet@gmail.com>
2024-09-05 02:22:45 +0530

    Run make generate

--- client-go/applyconfigurations/networking/v1alpha1/networkpolicyspec.go
+++ client-go/applyconfigurations/networking/v1alpha1/networkpolicyspec.go
@@ -7,8 +7,8 @@ package v1alpha1
 
 import (
 	networkingv1alpha1 "github.com/ironcore-dev/ironcore/api/networking/v1alpha1"
-	metav1 "github.com/ironcore-dev/ironcore/client-go/applyconfigurations/meta/v1"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/client-go/applyconfigurations/meta/v1"
 )
 
 // NetworkPolicySpecApplyConfiguration represents an declarative configuration of the NetworkPolicySpec type for use
