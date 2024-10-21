8ea3843fc587110dacc8094bf0ccd0a1b13522ab
Rohit <53218775+Rohit-0505@users.noreply.github.com>
2024-10-21 13:46:25 +0530

    Add e2e example for `Machine` creation using non ephemeral resources (#1133)
    * Add manifests to create a machine with non-ephemeral resources
    
    * update folder structure
    
    * increase `eventuallyTimeout` to 10s to avoid sporadic core_test failure
    
    * address review comments
    
    * update the `eventuallyTimeout`  with 3s in `app_suite_test.go`

---
+++ config/samples/e2e/bases/ignition/regenerate-ignition.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+butane -d . ignition.yaml | \
+  kubectl create secret generic ignition --from-file=ignition.yaml=/dev/stdin --dry-run=client -o yaml \
+  > ../ignition/ignition-secret.yaml
