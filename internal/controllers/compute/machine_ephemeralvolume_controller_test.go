03797786711fdedce31a60d0d95b12f80a0ec730
ushabelgur <usha.belgur@t-systems.com>
2024-11-14 14:47:37 +0530

    changes for introduce a reclaim policy for ephemerally created Volume resources

--- internal/controllers/compute/machine_ephemeralvolume_controller_test.go
+++ internal/controllers/compute/machine_ephemeralvolume_controller_test.go
@@ -10,10 +10,12 @@ import (
 	. "github.com/ironcore-dev/ironcore/utils/testing"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	. "sigs.k8s.io/controller-runtime/pkg/envtest/komega"
 )
 
@@ -52,7 +54,7 @@ var _ = Describe("MachineEphemeralVolumeController", func() {
 						VolumeSource: computev1alpha1.VolumeSource{
 							Ephemeral: &computev1alpha1.EphemeralVolumeSource{
 								VolumeTemplate: &storagev1alpha1.VolumeTemplateSpec{
-									Spec: storagev1alpha1.VolumeSpec{},
+									Spec: storagev1alpha1.EphemeralVolumeSpec{},
 								},
 							},
 						},
@@ -82,6 +84,13 @@ var _ = Describe("MachineEphemeralVolumeController", func() {
 			},
 		}
 		Eventually(Get(ephemVolume)).Should(Succeed())
+		By("Verifying OwnerRef is updated for ephemeral volume")
+		Expect(ephemVolume).Should(HaveField("ObjectMeta.OwnerReferences", ConsistOf(MatchFields(IgnoreExtras, Fields{
+			"APIVersion": Equal(computev1alpha1.SchemeGroupVersion.String()),
+			"Kind":       Equal("Machine"),
+			"Name":       Equal(machine.Name),
+		})),
+		))
 
 		By("asserting the referenced volume still exists")
 		Consistently(Get(refVolume)).Should(Succeed())
@@ -91,18 +100,6 @@ var _ = Describe("MachineEphemeralVolumeController", func() {
 	})
 
 	It("should not delete externally managed volumes for a machine", func(ctx SpecContext) {
-		By("creating a machine")
-		machine := &computev1alpha1.Machine{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace:    ns.Name,
-				GenerateName: "machine-",
-			},
-			Spec: computev1alpha1.MachineSpec{
-				MachineClassRef: corev1.LocalObjectReference{Name: machineClass.Name},
-			},
-		}
-		Expect(k8sClient.Create(ctx, machine)).To(Succeed())
-
 		By("creating an undesired controlled volume")
 		externalVolume := &storagev1alpha1.Volume{
 			ObjectMeta: metav1.ObjectMeta{
@@ -111,10 +108,88 @@ var _ = Describe("MachineEphemeralVolumeController", func() {
 			},
 			Spec: storagev1alpha1.VolumeSpec{},
 		}
-		_ = ctrl.SetControllerReference(machine, externalVolume, k8sClient.Scheme())
 		Expect(k8sClient.Create(ctx, externalVolume)).To(Succeed())
+
+		By("creating a machine")
+		machine := &computev1alpha1.Machine{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace:    ns.Name,
+				GenerateName: "machine-",
+			},
+			Spec: computev1alpha1.MachineSpec{
+				MachineClassRef: corev1.LocalObjectReference{Name: machineClass.Name},
+				Volumes: []computev1alpha1.Volume{
+					{
+						Name: "primary",
+						VolumeSource: computev1alpha1.VolumeSource{
+							VolumeRef: &corev1.LocalObjectReference{Name: externalVolume.Name},
+						},
+					},
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, machine)).To(Succeed())
 
 		By("asserting that the external volume is not being deleted")
 		Consistently(Object(externalVolume)).Should(HaveField("DeletionTimestamp", BeNil()))
+	})
+
+	It("verify ownerRef is set for ephemeral volumes based on reclaim policy", func(ctx SpecContext) {
+		By("creating a machine")
+		machine := &computev1alpha1.Machine{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace:    ns.Name,
+				GenerateName: "machine-",
+			},
+			Spec: computev1alpha1.MachineSpec{
+				MachineClassRef: corev1.LocalObjectReference{Name: machineClass.Name},
+				Volumes: []computev1alpha1.Volume{
+					{
+						Name: "ephem-volume",
+						VolumeSource: computev1alpha1.VolumeSource{
+							Ephemeral: &computev1alpha1.EphemeralVolumeSource{
+								VolumeTemplate: &storagev1alpha1.VolumeTemplateSpec{
+									Spec: storagev1alpha1.EphemeralVolumeSpec{
+										ReclaimPolicy: storagev1alpha1.Retain,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, machine)).To(Succeed())
+
+		By("waiting for the ephemeral volume to exist")
+		ephemVolume := &storagev1alpha1.Volume{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: ns.Name,
+				Name:      computev1alpha1.MachineEphemeralVolumeName(machine.Name, "ephem-volume"),
+			},
+		}
+		Eventually(Get(ephemVolume)).Should(Succeed())
+		By("Verifying OwnerRef is not set for ephemeral volume when reclaim policy is retain")
+		Eventually(Object(ephemVolume)).Should(HaveField("ObjectMeta.OwnerReferences", BeEmpty()))
+
+		By("Updating reclaim policy to delete")
+		baseMachine := machine.DeepCopy()
+		machineVolumes := machine.Spec.Volumes
+		for _, machineVolume := range machineVolumes {
+			if machineVolume.Ephemeral == nil {
+				continue
+			}
+			machineVolume.Ephemeral.VolumeTemplate.Spec.ReclaimPolicy = storagev1alpha1.Delete
+		}
+		machine.Spec.Volumes = machineVolumes
+		Expect(k8sClient.Patch(ctx, machine, client.MergeFrom(baseMachine))).To(Succeed())
+		By("Verifying OwnerRef is updated for ephemeral volume")
+		Eventually(Object(ephemVolume)).Should(HaveField("ObjectMeta.OwnerReferences", ConsistOf(MatchFields(IgnoreExtras, Fields{
+			"APIVersion": Equal(computev1alpha1.SchemeGroupVersion.String()),
+			"Kind":       Equal("Machine"),
+			"Name":       Equal(machine.Name),
+		})),
+		))
+
 	})
 })
