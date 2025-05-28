793d66a12703957060ccdf053010c5b195d79451
ushabelgur <usha.belgur@t-systems.com>
2025-05-28 15:13:08 +0530

    implement IRI version method in broker

---
+++ broker/bucketbroker/version/version.go
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package version
+
+const (
+	RuntimeName = "bucketbroker"
+)
+
+var (
+	Version string
+	Commit  string
+)
