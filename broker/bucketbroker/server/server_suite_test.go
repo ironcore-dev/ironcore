28eced12f14bbf5c2ec430ee53e3d1d5ba9a7288
Rohit <53218775+Rohit-0505@users.noreply.github.com>
2025-07-15 13:26:59 +0530

    Add `DownwardAPILabels` support for `Bucket` (#1324)

--- broker/bucketbroker/server/server_suite_test.go
+++ broker/bucketbroker/server/server_suite_test.go
@@ -16,6 +16,7 @@ import (
 	corev1alpha1 "github.com/ironcore-dev/ironcore/api/core/v1alpha1"
 	storagev1alpha1 "github.com/ironcore-dev/ironcore/api/storage/v1alpha1"
 	"github.com/ironcore-dev/ironcore/broker/bucketbroker/server"
+	bucketpoolletv1alpha1 "github.com/ironcore-dev/ironcore/poollet/bucketpoollet/api/v1alpha1"
 	utilsenvtest "github.com/ironcore-dev/ironcore/utils/envtest"
 	"github.com/ironcore-dev/ironcore/utils/envtest/apiserver"
 	. "github.com/onsi/ginkgo/v2"
@@ -137,6 +138,9 @@ func SetupTest() (*corev1.Namespace, *storagev1alpha1.BucketPool, *server.Server
 			BucketPoolName: bucketPool.Name,
 			BucketPoolSelector: map[string]string{
 				"pool": "test-pool",
+			},
+			BrokerDownwardAPILabels: map[string]string{
+				"root-bucket-uid": bucketpoolletv1alpha1.BucketUIDLabel,
 			},
 		})
 		Expect(err).NotTo(HaveOccurred())
